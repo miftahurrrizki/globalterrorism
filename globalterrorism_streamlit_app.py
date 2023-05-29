140ec61599713b063131568cbb4f5b762124d2a4
Miftahur Rizki <79993932+miftahurrrizki@users.noreply.github.com>
2023-05-29 17:17:01 +0700

    Update globalterrorism_streamlit_app.py

--- globalterrorism_streamlit_app.py
+++ globalterrorism_streamlit_app.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import streamlit as st
 import numpy as np
@@ -7,13 +8,20 @@ import random
 import folium
 from streamlit_folium import st_folium
 from streamlit_folium import folium_static
+from streamlit_echarts import st_echarts
+from pyecharts.charts import Pie
+from pyecharts import options as opts
+from palettable.colorbrewer.qualitative import Pastel1_5
+import plotly.graph_objects as go
+import colorlover as cl
 
 
 
 
 
 
-df = pd.read_csv(r"C:\Users\mfthr\anaconda3\envs\streamlit\Belajar Miftah\globalterrorism.csv", encoding="latin-1")
+
+df = pd.read_csv(r"C:\Users\mfthr\anaconda3\envs\streamlit\GlobalTerr\globalterrorism.csv", encoding="latin-1")
 
 #pre-processing
 
@@ -53,8 +61,7 @@ sub = 'Kelompok Yaudaaaaah'
 # )
 st.set_page_config(page_title = judul,
                    page_icon = ":skull:",
-                   layout = "wide")
-
+                   layout = "wide",initial_sidebar_state='collapsed')
 st.title(judul)
 st.caption(sub)
 
@@ -72,7 +79,7 @@ with column1:
     st.subheader(total_events)
 
 with column2:
-    st.subheader('Total Victims:')
+    st.subheader('Total Casualities:')
     st.subheader(int(total_victims))
 
 with column3:
@@ -80,25 +87,52 @@ with column3:
     st.subheader(len(total_groupnamex))
 
 
-
 # sidebar
+# st.sidebar.header("Silahkan Filter Disini:")
+# selected_countries = st.sidebar.multiselect('Select Country(s)', options=[
+#     'Lihat Seluruh Negara'] + list(df['Country'].unique())) #, default=['Lihat Seluruh Negara'])
+
+# selected_years = st.sidebar.multiselect('Select Year(s)', options=[
+#     'Lihat Seluruh Tahun'] + list(df['Year'].unique())) #, default=['Lihat Seluruh Tahun'])
+
+# # Menerapkan filter pada dataframe berdasarkan pilihan pengguna
+# if 'Lihat Seluruh Negara' in selected_countries:
+#     filtered_df = df[df['Country'].isin(df['Country'])]
+# elif 'Lihat Seluruh Tahun' in selected_years:
+#     filtered_df = df[df['Year'].isin(df['Year'])]
+# else:
+#     filtered_df = df[
+#         (df['Year'].isin(selected_years)) &
+#         (df['Country'].isin(selected_countries))
+#     ]
+
+# ----------SIDEBAR----------
 st.sidebar.header("Silahkan Filter Disini:")
 selected_countries = st.sidebar.multiselect('Select Country(s)', options=[
-    'Lihat Seluruh Negara'] + list(df['Country'].unique())) #, default=['Lihat Seluruh Negara'])
+    'Lihat Seluruh Negara'] + list(df['Country'].unique()))
 
-selected_years = st.sidebar.multiselect('Select Year(s)', options=[
-    'Lihat Seluruh Tahun'] + list(df['Year'].unique())) #, default=['Lihat Seluruh Tahun'])
+# selected_years = st.sidebar.multiselect('Select Year(s)', options=[
+#     'Lihat Seluruh Tahun'] + list(df['Year'].unique()))
 
+# selected_weapon = st.sidebar.multiselect('Select Weapon Type', options=[
+#     'Lihat Seluruh Senjata'] + list(df['WeaponType'].unique()))
+
+
+# ----------FILTER UTAMA----------
 # Menerapkan filter pada dataframe berdasarkan pilihan pengguna
-if 'Lihat Seluruh Negara' in selected_countries:
-    filtered_df = df[df['Country'].isin(df['Country'])]
-elif 'Lihat Seluruh Tahun' in selected_years:
-    filtered_df = df[df['Year'].isin(df['Year'])]
-else:
-    filtered_df = df[
-        (df['Year'].isin(selected_years)) &
-        (df['Country'].isin(selected_countries))
-    ]
+# if 'Lihat Seluruh Negara' in selected_countries and 'Lihat Seluruh Tahun' in selected_years and 'Lihat Seluruh Senjata' in selected_weapon:
+#     filtered_df = df.copy()  # Tampilkan semua data jika kedua opsi dipilih
+# else:
+#     if 'Lihat Seluruh Negara' in selected_countries:
+#         filtered_df = df[df['Year'].isin(selected_years)]
+#     elif 'Lihat Seluruh Tahun' in selected_years:
+#         filtered_df = df[df['Country'].isin(selected_countries)]
+#     else:
+#         filtered_df = df[
+#             (df['Year'].isin(selected_years)) &
+#             (df['Country'].isin(selected_countries)) &
+#             (df['WeaponType'].isin(selected_weapon))
+#         ]
 
 
 
@@ -107,7 +141,7 @@ else:
 #     st.write(filtered_df.shape)
 #     st.dataframe(filtered_df, use_container_width=True)
 
-# # Menerapkan filter pada dataframe berdasarkan pilihan pengguna
+# Menerapkan filter pada dataframe berdasarkan pilihan pengguna
 # if 'Lihat Seluruh Negara' in selected_countries:
 #     filtered_df = df[df['Country'].isin(selected_countries)]
 # elif 'Lihat Seluruh Tahun' in selected_years:
@@ -117,66 +151,19 @@ else:
 #         (df['Year'].isin(selected_years)) &
 #         (df['Country'].isin(selected_countries))
 #     ]
-
-
-# Menghitung total aksi teroris per negara setelah diterapkan filter
-country_counts = filtered_df['Country'].value_counts()
-
-# Mengurutkan berdasarkan total aksi teroris secara menurun
-country_counts_sorted = country_counts.sort_values(ascending=False)
-
-# Membuat dataframe baru dengan data yang terurut
-df_sorted = pd.DataFrame({'Country': country_counts_sorted.index,
-                         'Total Attacks': country_counts_sorted.values})
-
-# Membuat histogram menggunakan plotly
-fig = px.histogram(df_sorted, x='Country', y='Total Attacks',
-                   title=f'Total Aksi Teroris per Negara - Years: {", ".join(str(year) for year in selected_years)}, Countries: {", ".join(selected_countries)}',
-                   labels={'Country': 'Negara',
-                           'Total Attacks': 'Total Aksi Teroris'},
-                   nbins=len(df_sorted))
-
-# Mengurutkan histogram dari terbesar
-fig.update_xaxes(categoryorder='total descending')
-
-# Menampilkan histogram menggunakan Streamlit
-st.plotly_chart(fig, use_container_width=True)
-
-
-with st.container():
-    # Menghitung jumlah aksi terorisme dari tahun ke tahun
-    terrorism_count = filtered_df.groupby('Year').size()
-
-    # Membuat line chart menggunakan matplotlib
-    plt.figure(figsize=(12, 6))
-    plt.plot(terrorism_count.index, terrorism_count.values, marker='o')
-    plt.title('Perkembangan Jumlah Aksi Terorisme dari Tahun ke Tahun')
-    plt.xlabel('Tahun')
-    plt.ylabel('Jumlah Aksi Terorisme')
-    plt.grid(True)
-
-    # Menampilkan line chart menggunakan Streamlit
-    st.pyplot(plt)
-
-
-# # Menerapkan filter pada dataframe berdasarkan pilihan pengguna
-# if 'Lihat Seluruh Negara' in selected_countries:
-#     filtered_df = df[df['Country'].isin(selected_countries)]
+if 'Lihat Seluruh Negara' in selected_countries:
+    filtered_df = df
 # elif 'Lihat Seluruh Tahun' in selected_years:
-#     filtered_df = df[df['Year'].isin(selected_years)]
-# else:
-#     filtered_df = df[
-#         (df['Year'].isin(selected_years)) &
-#         (df['Country'].isin(selected_countries))
-#     ]
-
-
-# Menampilkan dataframe
-st.write(filtered_df[['Year', 'Month', 'Day', 'Region', 'Country', 'City', 'Latitude', 'Longitude', 'GroupName', 'AttackType',
-                          'WeaponType', 'Target', 'TargetType', 'Success', 'Suicide', 'Killed', 'Wounded']])
+#     filtered_df = df
+else:
+    filtered_df = df[
+        # (df['Year'].isin(selected_years)) &
+        (df['Country'].isin(selected_countries))
+    ]
 
 
 
+# ----------MAP PERSEBARAN----------
 with st.container():
     'Map Persebaran Aksi Terorisme'
     # Mendapatkan nilai rata-rata latitude dan longitude dari data yang telah difilter
@@ -184,13 +171,13 @@ with st.container():
     avg_longitude = filtered_df['Longitude'].mean()
 
     # Membuat peta dengan zoom awal pada titik latitude dan longitude yang dipilih
-    map = folium.Map(location=[avg_latitude, avg_longitude], zoom_start=3)
+    map = folium.Map()#location=[avg_latitude, avg_longitude], zoom_start=3)
 
     # Menambahkan marker pada peta untuk setiap baris dalam dataframe yang memiliki data koordinat
     for index, row in filtered_df.iterrows():
         if pd.notnull(row['Latitude']) and pd.notnull(row['Longitude']):
             location = [row['Latitude'], row['Longitude']]
-            total_casualties = row['Killed'] + row['Wounded']
+            total_casualties = int(row['Killed'] + row['Wounded'])
             group_name = row['GroupName']
 
             tooltip = f"Group: {group_name}<br>Total Casualties: {total_casualties}"
@@ -198,24 +185,258 @@ with st.container():
             folium.Marker(location, tooltip=tooltip).add_to(map)
 
     # Menampilkan peta di Streamlit
-    folium_static(map)
+    folium_static(map, width=1300, height=450)
+
+# ----------JML AKSI PER NEGARA----------
+with st.container():
+    
+    
+    # Menghitung total aksi teroris per negara setelah diterapkan filter
+    country_counts = filtered_df['Country'].value_counts()
+
+    # Mengurutkan berdasarkan total aksi teroris secara menurun
+    country_counts_sorted = country_counts.sort_values(ascending=False)
+
+    # Membuat dataframe baru dengan data yang terurut
+    df_sorted = pd.DataFrame({'Country': country_counts_sorted.index,
+                            'Total Attacks': country_counts_sorted.values})
+    
+    # Membuat histogram menggunakan plotly
+    fig = px.histogram(df_sorted, x='Country', y='Total Attacks',
+                       title=f'Total Aksi Teroris per Negara - Years: {", ".join(str(year) for year in selected_years)}, Countries: {", ".join(selected_countries)}',
+                       labels={'Country': 'Negara',
+                               'Total Attacks': 'Total Aksi Teroris'},
+                       text_auto='.2s',
+                       nbins=len(df_sorted))
+
+    # Mengurutkan histogram dari terbesar
+    fig.update_xaxes(categoryorder='total descending')
+
+    # Menampilkan histogram menggunakan Streamlit
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
+
+# ----------BARCHART KILLED N WOUNDED----------
+with st.container():
+    # Mengelompokkan data berdasarkan tahun dan menghitung total korban tewas dan terluka
+    filtered_df_grouped = filtered_df.groupby(
+        'Year')[['Killed', 'Wounded']].sum().reset_index()
+
+    # Membuat bar chart menggunakan plotly
+    fig = px.bar(filtered_df_grouped, x='Year', y=['Killed', 'Wounded'], barmode='group',
+                 color_discrete_sequence=['lightsalmon', 'lightblue'],
+                 
+                 title='Total Korban Tewas dan Terluka per Tahun')
+    fig.update_layout(xaxis_title='Tahun',
+                      yaxis_title='Jumlah', legend_title='')
+
+    # Menampilkan bar chart menggunakan Streamlit
+    st.plotly_chart(fig, use_container_width=True)
+
+
+# ----------LINECHART TAHUN KE TAHUN----------
+with st.container():
+
+    # Buat st.slider untuk memilih rentang tahun
+    year_range = st.slider('Filter by Year Range', min_value=int(df['Year'].min()), max_value=int(df['Year'].max()), value=(int(df['Year'].min()), int(df['Year'].max())))
+
+    # Ambil data yang telah difilter berdasarkan rentang tahun
+    filtered_data = df[(df['Country'].isin(selected_countries)) & (df['Year'].between(year_range[0], year_range[1]))]
+
+    # Buat dataframe baru untuk menghitung jumlah total aksi tahun ke tahun
+    total_actions = filtered_data.groupby('Year').size().reset_index(name='Total Actions')
+
+    # Konfigurasi grafik line race dengan efek hover
+    chart_config = {
+        "xAxis": {"type": "category", "data": total_actions['Year'].astype(str).tolist()},
+        "yAxis": {"type": "value"},
+        "series": [
+            {
+                "type": "line",
+                "data": total_actions['Total Actions'].tolist(),
+                "emphasis": {
+                    "focus": "series",
+                    "lineStyle": {"opacity": 1, "width": 4},
+                    "label": {"show": False},
+                },
+                "itemStyle": {"normal": {"opacity": 0.7, "width": 2}},
+            }
+        ],
+        "tooltip": {"trigger": "axis"},
+    }
+
+    # Tampilkan grafik menggunakan st_echarts
+    st_echarts(chart_config, height=600)
+
+
+# ----------LINERACE NEGARA PERTAHUN----------
+with st.container():
+
+    #     # Ambil tahun terkecil dan terbesar dari filtered_data
+    # min_year = filtered_data['Year'].min()
+    # max_year = filtered_data['Year'].max()
+
+    # # Slider untuk memilih rentang tahun
+    # selected_year_range = st.slider(
+    #     'Select Year Range', min_value=min_year, max_value=max_year, value=(min_year, max_year))
+
+    # # Menerapkan filter tahun pada filtered_data
+    # filtered_data = filtered_data[(filtered_data['Year'] >= selected_year_range[0]) & (
+    #     filtered_data['Year'] <= selected_year_range[1])]
+
+    # Buat dataframe baru untuk menghitung jumlah total aksi tahun ke tahun per negara
+    total_actions = filtered_data.groupby(
+        ['Year', 'Country']).size().reset_index(name='Total Actions')
+
+    # Buat list kosong untuk menyimpan series garis per negara
+    series_data = []
+
+    # Loop melalui setiap negara
+    for country in selected_countries:
+        country_data = total_actions[total_actions['Country'] == country]
+        series = {
+            "name": country,
+            "type": "line",
+            "data": country_data['Total Actions'].tolist(),
+            "emphasis": {
+                "focus": "series",
+                "lineStyle": {"opacity": 1, "width": 4},
+                "label": {"show": False},
+            },
+            "itemStyle": {"normal": {"opacity": 0.7, "width": 2}},
+        }
+        series_data.append(series)
+
+    # Konfigurasi grafik line race dengan efek hover
+    chart_config = {
+        "title": {"text": "Total Actions by Year"},
+        "xAxis": {"type": "category", "data": total_actions['Year'].astype(str).unique().tolist()},
+        "yAxis": {"type": "value"},
+        "series": series_data,
+        "tooltip": {"trigger": "axis"},
+    }
+
+    # Tampilkan grafik menggunakan st_echarts
+    st_echarts(chart_config, height=500)
 
 
 
 
 
+    # # Ambil data yang telah difilter sebelumnya
+    # filtered_data = df[df['Country'].isin(selected_countries)]
 
-# if st.container():
-#     # Mengelompokkan data berdasarkan tahun dan menghitung total korban tewas dan terluka
-#     df_dipilih_grouped = df_dipilih.groupby('Year')[['Killed', 'Wounded']].sum().reset_index()
+    # # Buat dataframe baru untuk menghitung jumlah total aksi tahun ke tahun per negara
+    # total_actions = filtered_data.groupby(['Year', 'Country']).size().reset_index(name='Total Actions')
 
-#     # Membuat bar chart menggunakan plotly
-#     fig = px.bar(df_dipilih_grouped, x='Year', y=['Killed', 'Wounded'], barmode='group',
-#                 title='Total Korban Tewas dan Terluka per Tahun')
-#     fig.update_layout(xaxis_title='Tahun', yaxis_title='Jumlah', legend_title='')
+    # # Buat list kosong untuk menyimpan series garis per negara
+    # series_data = []
 
-#     # Menampilkan bar chart menggunakan Streamlit
-#     st.plotly_chart(fig, use_container_width=True)
+    # # Loop melalui setiap negara
+    # for country in selected_countries:
+    #     country_data = total_actions[total_actions['Country'] == country]
+    #     series = {
+    #         "name": country,
+    #         "type": "line",
+    #         "data": country_data['Total Actions'].tolist(),
+    #         "emphasis": {
+    #             "focus": "series",
+    #             "lineStyle": {"opacity": 1, "width": 4},
+    #             "label": {"show": False},
+    #         },
+    #         "itemStyle": {"normal": {"opacity": 0.7, "width": 2}},
+    #     }
+    #     series_data.append(series)
+
+    # # Konfigurasi grafik line race dengan efek hover
+    # chart_config = {
+    #     "title": {"text": "Total Actions by Year"},
+    #     "xAxis": {"type": "category", "data": total_actions['Year'].astype(str).unique().tolist()},
+    #     "yAxis": {"type": "value"},
+    #     "series": series_data,
+    #     "tooltip": {"trigger": "axis"},
+    # }
+
+    # # Tampilkan grafik menggunakan st_echarts
+    # st_echarts(chart_config, height=500)
+
+
+
+# ----------HIST KILLED N WOUNDED----------
+with st.container():
+    # Membuat histogram menggunakan Plotly Express
+    fig = px.histogram(filtered_df, x=['Killed', 'Wounded'], nbins=50,
+                    color_discrete_sequence=['lightsalmon', 'lightblue'],
+                    labels={'value': 'Jumlah', 'variable': 'Kategori'})
+
+    # Menambahkan judul dan mengatur sumbu x dan y
+    fig.update_layout(title='Histogram Jumlah Korban Tewas dan Terluka',
+                    xaxis_title='Jumlah Korban',
+                    yaxis_title='Jumlah Kejadian')
+
+    # Menampilkan histogram menggunakan Streamlit
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
+
+
+# ----------DATAFRAME----------
+with st.container():
+    # Menampilkan dataframe
+    if st.checkbox("Show The Table"):
+        st.write(filtered_df[['Year', 'Month', 'Day', 'Region', 'Country', 'City', 'Latitude', 'Longitude', 'GroupName', 'AttackType',
+                              'WeaponType', 'Target', 'TargetType', 'Success', 'Suicide', 'Killed', 'Wounded']].shape)
+        
+        st.write(filtered_df[['Year', 'Month', 'Day', 'Region', 'Country', 'City', 'Latitude', 'Longitude', 'GroupName', 'AttackType',
+                              'WeaponType', 'Target', 'TargetType', 'Success', 'Suicide', 'Killed', 'Wounded']])
+
+
+with st.container():
+    
+    # Menghitung total aksi teroris per jenis senjata setelah diterapkan filter
+    weapon_counts = filtered_df['WeaponType'].value_counts()
+
+    # Mengurutkan berdasarkan total aksi teroris secara menurun
+    weapon_counts_sorted = weapon_counts.sort_values(ascending=False)
+
+    # Membuat dataframe baru dengan data yang terurut
+    df_sorted = pd.DataFrame({'WeaponType': weapon_counts_sorted.index,
+                            'Total Attacks': weapon_counts_sorted.values})
+    
+    
+    
+    # Membuat histogram menggunakan plotly
+    fig = px.histogram(df_sorted, x='WeaponType', y='Total Attacks',
+                       title=f'Total Aksi Teroris Berdasarkan Jenis Senjata - Years: {", ".join(str(year) for year in selected_years)}, Weapons: {", ".join(selected_weapon)}',
+                       labels={'WeaponType': 'Senjata',
+                               'Total Attacks': 'Total Aksi'},
+                       text_auto='.2s',
+                       nbins=len(df_sorted))
+
+    # Mengurutkan histogram dari terbesar
+    fig.update_xaxes(categoryorder='total descending')
+
+    # Menampilkan histogram menggunakan Streamlit
+    st.plotly_chart(fig, use_container_width=True)
+
+
+with st.container():
+    # Mengambil data WeaponType dan menghitung jumlahnya
+    weapon_counts = filtered_df['WeaponType'].value_counts().reset_index()
+    weapon_counts.columns = ['WeaponType', 'Count']
+
+    # Membuat pie chart menggunakan Plotly Express
+    fig = px.pie(weapon_counts, values='Count', names='WeaponType', title='Total Weapon Type')
+
+    # Menampilkan pie chart pada Streamlit
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
+
 
 
 # # Menampilkan filter untuk kolom 'Year' dan 'Country'
